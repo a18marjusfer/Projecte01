db11951f31800acb1e6fbbbca817f8afd9d1f538
a18marjusfer <57373391+a18marjusfer@users.noreply.github.com>
2019-11-12 17:09:11 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,10 +10,11 @@ if [ $(id -u) -eq 0 ]; then # 1-Make sure the script is being executed with supe
 	else
 		useradd -m -c $nomr $nom # 5-Create the user / 7-Set the password.
         [ $? -eq 0 ] && echo "L'usuari ha estat afegit al sistema." || echo "No s'ha pogut afegir l'usuari al sistema."
-        echo -e "$contra\n$contra" | passwd $nom # 7-Set the password. # 8-Check to see if the passwd command succeeded.
+        echo -e "$contra\n$contra" | passwd $nom # 7-Set the password. 
         passwd -e $nom  #9-Force password change on first login.
+        [ $? -eq 0 ] && echo "La contrasenya d'ha establert" || echo "No s'ha pogut establir la contrasenya." # 8-Check to see if the passwd command succeeded.
         x = `hostname`
-        echo" El usuari és $user, la contrasenya és $contra i el host és $x."
+        echo " El usuari és $user, la contrasenya és $contra i el host és $x." # 10-Display the username, password, and the host where the user was created.
 	fi
 else
 	echo "Nomès pots crear usuaris en mode Super Usuari"
