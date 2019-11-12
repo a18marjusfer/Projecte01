1c816ef8e4b507eb6fd7b25ac4c0ddab05418c20
a18marjusfer <57373391+a18marjusfer@users.noreply.github.com>
2019-11-12 17:13:35 +0100

    Update script.sh
    Super hiper alfa mega zafiro omega versión

--- script.sh
+++ script.sh
@@ -13,7 +13,7 @@ if [ $(id -u) -eq 0 ]; then # 1-Make sure the script is being executed with supe
         echo -e "$contra\n$contra" | passwd $nom # 7-Set the password. 
         passwd -e $nom  #9-Force password change on first login.
         [ $? -eq 0 ] && echo "La contrasenya d'ha establert" || echo "No s'ha pogut establir la contrasenya." # 8-Check to see if the passwd command succeeded.
-        echo " El usuari és $user, la contrasenya és $contra i el host és $HOSTNAME." # 10-Display the username, password, and the host where the user was created.
+        echo " El usuari és $nom, la contrasenya és $contra i el host és $HOSTNAME." # 10-Display the username, password, and the host where the user was created.
 	fi
 else
 	echo "Nomès pots crear usuaris en mode Super Usuari"
