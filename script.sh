79f3704563f794ccf6c3c712fd4bdfc8b0128ad1
a18marjusfer <57373391+a18marjusfer@users.noreply.github.com>
2019-11-12 16:26:46 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,16 +2,14 @@
 if [ $(id -u) -eq 0 ]; then # 1-Make sure the script is being executed with superuser privileges.
 	read -p "Introdueix l'usuari : " nom # 2-Get the username (login).
     read -p "Introdueix el nom real : " nomr # 3-Get the real name (contents for the description field).
-	read -s -p "Introdueix la contrasenya : " contra # 4-Get the password.
 	egrep "^$nom" /etc/passwd >/dev/null 2>&1
 	if [ $? -eq 0 ]; then
 		echo "L'usuari $nom existeix!"
 		exit 1
 	else
-		useradd -m -c $nomr $nom # 5-Create the user / 7-Set the password.
+		useradd -m -c $nomr $nom # 5-Create the user / 
         [ $? -eq 0 ] && echo "L'usuari ha estat afegit al sistema." || echo "No s'ha pogut afegir l'usuari al sistema."
-        passwd $nom 
-        $contra
+        passwd $nom #7-Set the password.
 	fi
 else
 	echo "Nomès pots crear usuaris en mode Super Usuari"
