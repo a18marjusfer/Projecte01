59d83c3926827f3829e1cfea8461f15a57b156b0
freewhiskys <57413343+freewhiskys@users.noreply.github.com>
2019-11-13 18:50:14 +0100

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ $(id -u) -eq 0 ]; then # 1-Make sure the script is being executed with superuser privileges.
+	read -p "Introdueix l'usuari : " nom # 2-Get the username (login).
+    read -p "Introdueix el nom real : " nomr # 3-Get the real name (contents for the description field).
+	read -s -p "Introdueix la contrasenya : " contra # 4-Get the password.
+	egrep "^$nom" /etc/passwd >/dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		echo "L'usuari $nom existeix!"
+		exit 1
+	else
+		useradd -m -c $nomr $nom # 5-Create the user / 7-Set the password.
